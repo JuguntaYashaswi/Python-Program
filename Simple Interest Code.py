1a42712d1f5ecbb6afaa81258caa5e56d6c594e4
JuguntaYashaswi <96945816+JuguntaYashaswi@users.noreply.github.com>
2022-01-02 13:26:18 +0530

    Add files via upload

---
+++ Simple Interest Code.py
@@ -0,0 +1,10 @@
+print("")
+p= int(input("Enter The Principal: "))
+t= int(input("Enter The Time In Month: "))
+r= int(input("Enter The Rate Of Interest: "))
+
+si= (p*t*r)/100
+
+print("The Simple Interest Is: ",si)
+print("")
+
